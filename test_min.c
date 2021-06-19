51b6a4467ca1a17371802642671ae12d123f36eb
Malavika-R <66917713+Malavika-R@users.noreply.github.com>
2021-06-19 13:27:47 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -3,16 +3,6 @@
 
 
 int min(int [], int);
-int min(int A[], int n)
-{
-    int m = A[0], temp;
-    for(int i=1; i<n ; i++)
-    {
-        if(A[i]<=m)
-            m= A[i];
-    }
-    return m;
-}
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
