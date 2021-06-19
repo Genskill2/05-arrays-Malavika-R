bc3011ca46f53b443937b1d767b52f6aab7409b2
Malavika-R <66917713+Malavika-R@users.noreply.github.com>
2021-06-19 13:29:12 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -4,25 +4,6 @@
 
 int mode(int [], int);
 
-int mode(int A[], int n)
-{
-    int maxcount=0;
-    for(int i=0;i<n;i++)
-    {
-        int count=0;
-        for(int j=0;j<n;j++)
-        {
-            if(A[j]==A[i])
-                ++count;
-        }
-        if(count>maxcount)
-        {
-            maxcount=count;
-        }
-        return A[i];
-    }
-}
-
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
   assert ((mode(x, 12) == 4));
