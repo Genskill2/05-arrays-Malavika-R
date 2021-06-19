44fd2666224513fa66d953f093a2069d59c71a09
Malavika-R <66917713+Malavika-R@users.noreply.github.com>
2021-06-19 13:10:51 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -4,6 +4,38 @@
 
 int factors(int, int []);
 
+int factors(int n, int A[])
+{
+    int i=0, count =0;
+    while (n%2==0)
+    {
+        count++;
+        A[i++]=2;
+    }
+     while (n%3==0)
+    {
+        count++;
+        A[i++]=3;
+    }
+     while (n%5==0)
+    {
+        count++;
+        A[i++]=5;
+    }
+     while (n%11==0)
+    {
+        count++;
+        A[i++]=11;
+    }
+     while (n%13==0)
+    {
+        count++;
+        A[i++]=13;
+    }
+    return count;
+}
+
+
 int main(void) {
   int ret[100] = {0};
   int count = factors(180, ret);
